f9e2e443ea3e304d9f261cc11c48c5a02c85bd8d
Uzair Ahmed <essani.addy@gmail.com>
2025-04-26 11:20:26 +0500

    files

---
+++ extra.py
@@ -0,0 +1,12 @@
+
+
+
+# def f(x):
+#     def f1(*args, **kwargs):
+#            print("Sanfoundry")
+#            return x(*args, **kwargs)
+#     return f1
+
+# f("Uzair")
+
+min(max(False,-3,-4), 2,7)
